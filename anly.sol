370c99b3607661cc7871dd689e0efb7f383ce4a7
Ogar segun <ogarstephen98@gmail.com>
2024-01-23 16:22:41 +0100

    Borrow function working

---
+++ anly.sol
@@ -0,0 +1,353 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+pragma solidity ^0.8.19;
+
+//use oppenzeppelin
+import "solmate/utils/ReentrancyGuard.sol";
+import "solmate/auth/Owned.sol";
+import "solmate/tokens/ERC4626.sol";
+import "solmate/utils/SafeTransferLib.sol";
+import "solmate/utils/FixedPointMathLib.sol";
+import "./VaultBase.sol";
+
+/// @title VaultSimple
+/// @dev It provides basic functionality for vaults.
+/// @notice In this contract, the EVC is authenticated before any action that may affect the state of the vault or an
+/// account. This is done to ensure that if it's EVC calling, the account is correctly authorized. Unlike solmate,
+/// VaultSimple implementation prevents from share inflation attack by using virtual assets and shares. Look into
+/// Open-Zeppelin documentation for more details. This contract does not take the supply cap into account when
+/// calculating max deposit and max mint values.
+contract VaultSimple is VaultBase, ReentrancyGuard, Owned, ERC4626 {
+    //safeTransferLib  for anything
+    using SafeTransferLib for ERC20;
+    using FixedPointMathLib for uint256;
+//setting supply so as to use vault status check
+    event SupplyCapSet(uint256 newSupplyCap);
+
+ //take the snapshot of what?
+    error SnapshotNotTaken();
+    error SupplyCapExceeded();
+
+    uint256 public supplyCap;
+
+    constructor(
+        IEVC _evc,
+        ERC20 _asset,
+        string memory _name,
+        string memory _symbol
+    ) VaultBase(_evc) Owned(msg.sender) ERC4626(_asset, _name, _symbol) {}
+
+    /// @notice Sets the supply cap of the vault.
+    /// @param newSupplyCap The new supply cap.
+
+    //@audit we set the supply cap here
+    function setSupplyCap(uint256 newSupplyCap) external onlyOwner {
+        supplyCap = newSupplyCap;
+        emit SupplyCapSet(newSupplyCap);
+    }
+
+    /// @notice Creates a snapshot of the vault.
+    /// @dev This function is called before any action that may affect the vault's state.
+    /// @return A snapshot of the vault's state.
+    //@audit take snapshot of the supply in the vault
+    function doCreateVaultSnapshot() internal virtual override returns (bytes memory) {
+        // make total supply snapshot here and return it:
+        return abi.encode(_convertToAssets(totalSupply, false));
+    }
+
+    /// @notice Checks the vault's status.
+    /// @dev This function is called after any action that may affect the vault's state.
+    /// @param oldSnapshot The snapshot of the vault's state before the action.
+    
+    //@audit basically check for supply to simulate healthy vault status
+    function doCheckVaultStatus(bytes memory oldSnapshot) internal virtual override {
+        // sanity check in case the snapshot hasn't been taken
+        if (oldSnapshot.length == 0) revert SnapshotNotTaken();
+
+        // validate the vault state here:
+        uint256 initialSupply = abi.decode(oldSnapshot, (uint256));
+        uint256 finalSupply = _convertToAssets(totalSupply, false);
+
+        // the supply cap can be implemented like this:
+        if (supplyCap != 0 && finalSupply > supplyCap && finalSupply > initialSupply) {
+            revert SupplyCapExceeded();
+        }
+    }
+
+    /// @notice Checks the status of an account.
+    /// @dev This function is called after any action that may affect the account's state.
+    function doCheckAccountStatus(address, address[] calldata) internal view virtual override {
+        // no need to do anything here because the vault does not allow borrowing
+    }
+
+    /// @notice Disables the controller.
+    /// @dev The controller is only disabled if the account has no debt.
+    function disableController() external virtual override nonReentrant {
+        // this vault doesn't allow borrowing, so we can't check that the account has no debt.
+        // this vault should never be a controller, but user errors can happen
+        EVCClient.disableController(_msgSender());
+    }
+
+    /// @notice Returns the total assets of the vault.
+    /// @return The total assets.
+    function totalAssets() public view virtual override returns (uint256) {
+        return asset.balanceOf(address(this));
+    }
+
+    /// @notice Converts assets to shares.
+    /// @dev That function is manipulable in its current form as it uses exact values. Considering that other vaults may
+    /// rely on it, for a production vault, a manipulation resistant mechanism should be implemented.
+    /// @param assets The assets to convert.
+    /// @return The converted shares.
+    function convertToShares(uint256 assets) public view virtual override returns (uint256) {
+        return _convertToShares(assets, false);
+    }
+
+    /// @notice Converts shares to assets.
+    /// @dev That function is manipulable in its current form as it uses exact values. Considering that other vaults may
+    /// rely on it, for a production vault, a manipulation resistant mechanism should be implemented.
+    /// @param shares The shares to convert.
+    /// @return The converted assets.
+    function convertToAssets(uint256 shares) public view virtual override returns (uint256) {
+        return _convertToAssets(shares, false);
+    }
+
+    /// @notice Simulates the effects of depositing a certain amount of assets at the current block.
+    /// @param assets The amount of assets to simulate depositing.
+    /// @return The amount of shares that would be minted.
+    function previewDeposit(uint256 assets) public view virtual override returns (uint256) {
+        return _convertToShares(assets, false);
+    }
+
+    /// @notice Simulates the effects of minting a certain amount of shares at the current block.
+    /// @param shares The amount of shares to simulate minting.
+    /// @return The amount of assets that would be deposited.
+    function previewMint(uint256 shares) public view virtual override returns (uint256) {
+        return _convertToAssets(shares, true);
+    }
+
+    /// @notice Simulates the effects of withdrawing a certain amount of assets at the current block.
+    /// @param assets The amount of assets to simulate withdrawing.
+    /// @return The amount of shares that would be burned.
+    function previewWithdraw(uint256 assets) public view virtual override returns (uint256) {
+        return _convertToShares(assets, true);
+    }
+
+    /// @notice Simulates the effects of redeeming a certain amount of shares at the current block.
+    /// @param shares The amount of shares to simulate redeeming.
+    /// @return The amount of assets that would be redeemed.
+    function previewRedeem(uint256 shares) public view virtual override returns (uint256) {
+        return _convertToAssets(shares, false);
+    }
+
+    /// @notice Approves a spender to spend a certain amount.
+    /// @param spender The spender to approve.
+    /// @param amount The amount to approve.
+    /// @return A boolean indicating whether the approval was successful.
+    function approve(address spender, uint256 amount) public virtual override returns (bool) {
+        address msgSender = _msgSender();
+
+        allowance[msgSender][spender] = amount;
+
+        emit Approval(msgSender, spender, amount);
+
+        return true;
+    }
+
+    /// @notice Transfers a certain amount of shares to a recipient.
+    /// @param to The recipient of the transfer.
+    /// @param amount The amount shares to transfer.
+    /// @return A boolean indicating whether the transfer was successful.
+    function transfer(address to, uint256 amount) public virtual override callThroughEVC nonReentrant returns (bool) {
+        address msgSender = _msgSender();
+
+        createVaultSnapshot();
+
+        balanceOf[msgSender] -= amount;
+
+        // Cannot overflow because the sum of all user
+        // balances can't exceed the max uint256 value.
+        unchecked {
+            balanceOf[to] += amount;
+        }
+
+        emit Transfer(msgSender, to, amount);
+
+        // despite the fact that the vault status check might not be needed for shares transfer with current logic, it's
+        // added here so that if anyone changes the snapshot/vault status check mechanisms in the inheriting contracts,
+        // they will not forget to add the vault status check here
+        requireAccountAndVaultStatusCheck(msgSender);
+
+        return true;
+    }
+
+    /// @notice Transfers a certain amount of shares from a sender to a recipient.
+    /// @param from The sender of the transfer.
+    /// @param to The recipient of the transfer.
+    /// @param amount The amount of shares to transfer.
+    /// @return A boolean indicating whether the transfer was successful.
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public virtual override callThroughEVC nonReentrant returns (bool) {
+        address msgSender = _msgSender();
+
+        createVaultSnapshot();
+
+        uint256 allowed = allowance[from][msgSender]; // Saves gas for limited approvals.
+
+        if (allowed != type(uint256).max) {
+            allowance[from][msgSender] = allowed - amount;
+        }
+
+        balanceOf[from] -= amount;
+
+        // Cannot overflow because the sum of all user
+        // balances can't exceed the max uint256 value.
+        unchecked {
+            balanceOf[to] += amount;
+        }
+
+        emit Transfer(from, to, amount);
+
+        // despite the fact that the vault status check might not be needed for shares transfer with current logic, it's
+        // added here so that if anyone changes the snapshot/vault status check mechanisms in the inheriting contracts,
+        // they will not forget to add the vault status check here
+        requireAccountAndVaultStatusCheck(from);
+
+        return true;
+    }
+
+    /// @notice Deposits a certain amount of assets for a receiver.
+    /// @param assets The assets to deposit.
+    /// @param receiver The receiver of the deposit.
+    /// @return shares The shares equivalent to the deposited assets.
+    function deposit(
+        uint256 assets,
+        address receiver
+    ) public virtual override callThroughEVC nonReentrant returns (uint256 shares) {
+        address msgSender = _msgSender();
+
+        createVaultSnapshot();
+
+        // Check for rounding error since we round down in previewDeposit.
+        require((shares = previewDeposit(assets)) != 0, "ZERO_SHARES");
+
+        // Need to transfer before minting or ERC777s could reenter.
+        asset.safeTransferFrom(msgSender, address(this), assets);
+
+        _mint(receiver, shares);
+
+        emit Deposit(msgSender, receiver, assets, shares);
+
+        requireVaultStatusCheck();
+    }
+
+    /// @notice Mints a certain amount of shares for a receiver.
+    /// @param shares The shares to mint.
+    /// @param receiver The receiver of the mint.
+    /// @return assets The assets equivalent to the minted shares.
+    function mint(
+        uint256 shares,
+        address receiver
+    ) public virtual override callThroughEVC nonReentrant returns (uint256 assets) {
+        address msgSender = _msgSender();
+
+        createVaultSnapshot();
+
+        assets = previewMint(shares); // No need to check for rounding error, previewMint rounds up.
+
+        // Need to transfer before minting or ERC777s could reenter.
+        asset.safeTransferFrom(msgSender, address(this), assets);
+
+        _mint(receiver, shares);
+
+        emit Deposit(msgSender, receiver, assets, shares);
+
+        requireVaultStatusCheck();
+    }
+
+    /// @notice Withdraws a certain amount of assets for a receiver.
+    /// @param assets The assets to withdraw.
+    /// @param receiver The receiver of the withdrawal.
+    /// @param owner The owner of the assets.
+    /// @return shares The shares equivalent to the withdrawn assets.
+    function withdraw(
+        uint256 assets,
+        address receiver,
+        address owner
+    ) public virtual override callThroughEVC nonReentrant returns (uint256 shares) {
+        address msgSender = _msgSender();
+
+        createVaultSnapshot();
+
+        shares = previewWithdraw(assets); // No need to check for rounding error, previewWithdraw rounds up.
+
+        if (msgSender != owner) {
+            uint256 allowed = allowance[owner][msgSender]; // Saves gas for limited approvals.
+
+            if (allowed != type(uint256).max) {
+                allowance[owner][msgSender] = allowed - shares;
+            }
+        }
+
+        receiver = getAccountOwner(receiver);
+
+        _burn(owner, shares);
+
+        emit Withdraw(msgSender, receiver, owner, assets, shares);
+
+        asset.safeTransfer(receiver, assets);
+
+        requireAccountAndVaultStatusCheck(owner);
+    }
+
+    /// @notice Redeems a certain amount of shares for a receiver.
+    /// @param shares The shares to redeem.
+    /// @param receiver The receiver of the redemption.
+    /// @param owner The owner of the shares.
+    /// @return assets The assets equivalent to the redeemed shares.
+    function redeem(
+        uint256 shares,
+        address receiver,
+        address owner
+    ) public virtual override callThroughEVC nonReentrant returns (uint256 assets) {
+        address msgSender = _msgSender();
+
+        createVaultSnapshot();
+
+        if (msgSender != owner) {
+            uint256 allowed = allowance[owner][msgSender]; // Saves gas for limited approvals.
+
+            if (allowed != type(uint256).max) {
+                allowance[owner][msgSender] = allowed - shares;
+            }
+        }
+
+        // Check for rounding error since we round down in previewRedeem.
+        require((assets = previewRedeem(shares)) != 0, "ZERO_ASSETS");
+
+        receiver = getAccountOwner(receiver);
+
+        _burn(owner, shares);
+
+        emit Withdraw(msgSender, receiver, owner, assets, shares);
+
+        asset.safeTransfer(receiver, assets);
+
+        requireAccountAndVaultStatusCheck(owner);
+    }
+
+    function _convertToShares(uint256 assets, bool roundUp) internal view virtual returns (uint256) {
+        return roundUp
+            ? assets.mulDivUp(totalSupply + 1, totalAssets() + 1)
+            : assets.mulDivDown(totalSupply + 1, totalAssets() + 1);
+    }
+
+    function _convertToAssets(uint256 shares, bool roundUp) internal view virtual returns (uint256) {
+        return roundUp
+            ? shares.mulDivUp(totalAssets() + 1, totalSupply + 1)
+            : shares.mulDivDown(totalAssets() + 1, totalSupply + 1);
+    }
+}
--- src/workshop_2/WorkshopVault.sol
+++ src/workshop_2/WorkshopVault.sol
@@ -6,9 +6,27 @@ import "openzeppelin/token/ERC20/extensions/ERC4626.sol";
 import "evc/interfaces/IEthereumVaultConnector.sol";
 import "evc/interfaces/IVault.sol";
 import "./IWorkshopVault.sol";
+import {Math} from "../../openzeppelin/utils/math/Math.sol";
+import {ReentrancyGuard} from "../../openzeppelin/utils/ReentrancyGuard.sol";
+import {console} from "../../lib/forge-std/src/console.sol";
 
-contract WorkshopVault is ERC4626, IVault, IWorkshopVault {
+contract WorkshopVault is ERC4626, IVault, IWorkshopVault, ReentrancyGuard {
+    //error
+    error unAbleToBorrow();
+    using Math for uint;
+
+    //Storage variable
+    uint256 public s_totalAmountBorrowedFromVault;
+    bytes private takeSnapshot;
+    uint public totalBorrowedAssets;
+    uint internal _totalAssets;
+
+    mapping(address => uint256) public accountAccruedDebt;
+    mapping(address => uint256) public balanceOfShares;
+
     IEVC internal immutable evc;
+    IERC20 assetToBorrow;
+    uint collateralFactor = 80;
 
     constructor(
         IEVC _evc,
@@ -17,6 +35,7 @@ contract WorkshopVault is ERC4626, IVault, IWorkshopVault {
         string memory _symbol
     ) ERC4626(_asset) ERC20(_name, _symbol) {
         evc = _evc;
+        assetToBorrow = _asset;
     }
 
     // [ASSIGNMENT]: what is the purpose of this modifier?
@@ -24,7 +43,12 @@ contract WorkshopVault is ERC4626, IVault, IWorkshopVault {
         if (msg.sender == address(evc)) {
             _;
         } else {
-            bytes memory result = evc.call(address(this), msg.sender, 0, msg.data);
+            bytes memory result = evc.call(
+                address(this),
+                msg.sender,
+                0,
+                msg.data
+            );
 
             assembly {
                 return(add(32, result), mload(result))
@@ -35,6 +59,15 @@ contract WorkshopVault is ERC4626, IVault, IWorkshopVault {
     // [ASSIGNMENT]: why the account status check might not be necessary in certain situations?
     // [ASSIGNMENT]: is the vault status check always necessary? why?
     modifier withChecks(address account) {
+        //take the snapshot of the vault/account
+
+        //ALWAYS take the most recent snapshot of the vault by including "withChecks" modifier
+        //on any vault changing function
+        takeSnapshot = abi.encode(
+            _convertToAssets(totalAssets(), Math.Rounding.Floor),
+            totalBorrowedAssets
+        );
+
         _;
 
         if (account == address(0)) {
@@ -49,11 +82,43 @@ contract WorkshopVault is ERC4626, IVault, IWorkshopVault {
     // [ASSIGNMENT]: if the answer to the above is "no", how this function could be modified to allow safe borrowing?
     function _msgSender() internal view virtual override returns (address) {
         if (msg.sender == address(evc)) {
-            (address onBehalfOfAccount,) = evc.getCurrentOnBehalfOfAccount(address(0));
+            (address onBehalfOfAccount, ) = evc.getCurrentOnBehalfOfAccount(
+                address(0)
+            );
             return onBehalfOfAccount;
         } else {
             return msg.sender;
         }
+    }
+
+    function convertToAssets(uint256 shares)
+        public
+        view
+        virtual
+        override
+        returns (uint256)
+    {
+        return
+            shares.mulDiv(
+                totalAssets() + totalBorrowedAssets +1,
+                totalSupply() + 10**_decimalsOffset(),
+                Math.Rounding.Floor
+            );
+    }
+
+    function maxWithdraw(address owner)
+        public
+        view
+        virtual
+        override
+        returns (uint256)
+    {
+        uint256 totAssets = totalAssets();
+        uint256 ownerAssets = convertToAssets(
+            balanceOfShares[owner] 
+        );
+
+        return ownerAssets > totAssets ? totAssets : ownerAssets;
     }
 
     // IVault
@@ -63,14 +128,28 @@ contract WorkshopVault is ERC4626, IVault, IWorkshopVault {
     }
 
     // [ASSIGNMENT]: provide a couple use cases for this function
-    function checkAccountStatus(
-        address account,
-        address[] calldata collaterals
-    ) public virtual returns (bytes4 magicValue) {
+    function checkAccountStatus(address account, address[] calldata collaterals)
+        public
+        virtual
+        returns (bytes4 magicValue)
+    {
         require(msg.sender == address(evc), "only evc can call this");
-        require(evc.areChecksInProgress(), "can only be called when checks in progress");
+        require(
+            evc.areChecksInProgress(),
+            "can only be called when checks in progress"
+        );
+        //do you have an outstanding debt
+        uint accountDebt = accountAccruedDebt[account];
 
-        // some custom logic evaluating the account health
+        //logic to check if the account is healthy
+        //We can keep it minimal. However,for fully functional lending dapp, we will use oracle
+        //pricefeed to determine the value of the collaterals relative to the asset value we lend out
+
+        //loan value= collateral_factor * collateral_value
+        //Therefore, loan_value > debt ? accountHealthy: accountUnHealthyStatus;
+
+        /////////////////////////////////////HARDCODED/////////////////////////////
+        require(accountDebt < type(uint64).max, "unhealthy account");
 
         return IVault.checkAccountStatus.selector;
     }
@@ -78,35 +157,79 @@ contract WorkshopVault is ERC4626, IVault, IWorkshopVault {
     // [ASSIGNMENT]: provide a couple use cases for this function
     function checkVaultStatus() public virtual returns (bytes4 magicValue) {
         require(msg.sender == address(evc), "only evc can call this");
-        require(evc.areChecksInProgress(), "can only be called when checks in progress");
+        require(
+            evc.areChecksInProgress(),
+            "can only be called when checks in progress"
+        );
 
-        // some custom logic evaluating the vault health
+        /////////// SOME CUSTOM LOGIC EVALUATING VAULT HEALTH
 
-        // [ASSIGNMENT]: what can be done if the vault status check needs access to the initial state of the vault in
-        // order to evaluate the vault health?
+        //withChecks modifier must have taken the snapshot of the vault
+        require(takeSnapshot.length != 0, "snapShot must have been taken");
+
+        (uint256 sharesSupply, uint256 borrowLevel) = abi.decode(
+            takeSnapshot,
+            (uint256, uint256)
+        );
+        uint256 currentSupply = _convertToAssets(
+            totalSupply(),
+            Math.Rounding.Floor
+        );
+
+        ////CHECK IF currentSupply > maxSupply? revert
+        ////CHECK if borrowLevel > maxBorrow? revert
+
+        delete takeSnapshot;
 
         return IVault.checkVaultStatus.selector;
     }
 
-    function deposit(
-        uint256 assets,
-        address receiver
-    ) public virtual override callThroughEVC withChecks(address(0)) returns (uint256 shares) {
-        return super.deposit(assets, receiver);
+    function deposit(uint256 assets, address receiver)
+        public
+        virtual
+        override
+        callThroughEVC
+        withChecks(address(0))
+        returns (uint256 shares)
+    {
+        assetToBorrow.transferFrom(_msgSender(), address(this), assets);
+        _totalAssets += assets;
+        (shares) = _convertToShares(assets, Math.Rounding.Floor);
+        balanceOfShares[_msgSender()] += shares;
+
+        console.log(_msgSender());
+
+        //@audit evc is calling error
+        mint(shares, receiver);
     }
 
-    function mint(
-        uint256 shares,
-        address receiver
-    ) public virtual override callThroughEVC withChecks(address(0)) returns (uint256 assets) {
-        return super.mint(shares, receiver);
+    function totalAssets() public view virtual override returns (uint256) {
+        return _totalAssets;
+    }
+
+    function mint(uint256 shares, address receiver)
+        public
+        virtual
+        override
+        withChecks(address(_msgSender()))
+        returns (uint256 assets)
+    {
+        console.log(msg.sender);
+        _mint(receiver, shares);
     }
 
     function withdraw(
         uint256 assets,
         address receiver,
         address owner
-    ) public virtual override callThroughEVC withChecks(owner) returns (uint256 shares) {
+    )
+        public
+        virtual
+        override
+        callThroughEVC
+        withChecks(owner)
+        returns (uint256 shares)
+    {
         return super.withdraw(assets, receiver, owner);
     }
 
@@ -114,28 +237,107 @@ contract WorkshopVault is ERC4626, IVault, IWorkshopVault {
         uint256 shares,
         address receiver,
         address owner
-    ) public virtual override callThroughEVC withChecks(owner) returns (uint256 assets) {
+    )
+        public
+        virtual
+        override
+        callThroughEVC
+        withChecks(owner)
+        returns (uint256 assets)
+    {
         return super.redeem(shares, receiver, owner);
     }
 
-    function transfer(
-        address to,
-        uint256 value
-    ) public virtual override (ERC20, IERC20) callThroughEVC withChecks(_msgSender()) returns (bool) {
+    function transfer(address to, uint256 value)
+        public
+        virtual
+        override(ERC20, IERC20)
+        callThroughEVC
+        withChecks(_msgSender())
+        returns (bool)
+    {
         return super.transfer(to, value);
     }
 
-    function transferFrom(
-        address from,
-        address to,
-        uint256 value
-    ) public virtual override (ERC20, IERC20) callThroughEVC withChecks(from) returns (bool) {
-        return super.transferFrom(from, to, value);
+    // function transferFrom(
+    //     address from,
+    //     address to,
+    //     uint256 amount
+    // )
+    //     public
+    //     virtual
+    //     override(ERC20, IERC20)
+
+    //     returns (bool)
+    // {
+    //     super.transferFrom(from,to,amount);
+    // }
+
+    //override maxWithdraw
+    //  function maxWithdraw(address owner) public view virtual override returns (uint256) {
+    //     uint256 totAssets = totalAssets();
+    //     uint256 ownerAssets = _convertToAssets(balanceOf[owner], false);
+
+    //     return ownerAssets > totAssets ? totAssets : ownerAssets;
+    // }
+    function _convertToAssets(uint256 shares, Math.Rounding rounding)
+        internal
+        view
+        virtual
+        override
+        returns (uint256)
+    {
+        return shares.mulDiv(
+            totalAssets() + totalBorrowedAssets + 1,
+            totalSupply() + 10**_decimalsOffset(),
+            rounding
+        );
+    }
+
+    //     /// @dev This function is overridden to take into account the fact that some of the assets may be borrowed.
+    function _convertToShares(uint256 assets, Math.Rounding rounding)
+        internal
+        view
+        virtual
+        override
+        returns (uint256)
+    {
+        return assets;
+        // assets.mulDiv(
+        //     totalSupply() + 10**_decimalsOffset(),
+        //     totalAssets() + totalBorrowedAssets + 1,
+        //     rounding
+        // );
     }
 
     // IWorkshopVault
-    function borrow(uint256 assets, address receiver) external {}
+    function borrow(uint256 assets, address receiver)
+        external
+        callThroughEVC
+        withChecks(_msgSender())
+    {
+        //////////////////////CHECKS
+        if (!evc.isControllerEnabled(_msgSender(), address(this))) {
+            revert unAbleToBorrow();
+        }
+        require(assets != 0, "asset cannot be zero");
+
+        ////////////////////////UPDATE VAULT STATES
+
+        //check the vault status if it is healthy and take the snapshot
+
+        //emit event after
+
+        accountAccruedDebt[_msgSender()] += assets;
+        totalBorrowedAssets += assets;
+
+        //EXTERNAL CALLS
+        assetToBorrow.transfer(receiver, assets);
+    }
+
     function repay(uint256 assets, address receiver) external {}
+
     function pullDebt(address from, uint256 assets) external returns (bool) {}
+
     function liquidate(address violator, address collateral) external {}
 }
