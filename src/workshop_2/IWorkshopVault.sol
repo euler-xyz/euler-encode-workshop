53da33b12c34042da334c92df91228c749c508c1
Tanishq <100078125+Spydiecy@users.noreply.github.com>
2024-01-28 12:38:55 +0530

    Update IWorkshopVault.sol
    Signed-off-by: Tanishq <100078125+Spydiecy@users.noreply.github.com>

--- src/workshop_2/IWorkshopVault.sol
+++ src/workshop_2/IWorkshopVault.sol
@@ -2,28 +2,77 @@
 
 pragma solidity ^0.8.19;
 
-import "evc/interfaces/IVault.sol";
+contract WorkshopVault {
+    mapping(address => uint256) private balances;
+    mapping(address => uint256) private debts;
+    uint256 private totalAssets;
+    uint256 private totalShares;
+    bool private controllerEnabled = true;
+    address private lastBorrower;
 
-interface IWorkshopVault is IVault {
-    // [ASSIGNMENT]: add borrowing functionality by implementing the following functions:
-    function borrow(uint256 assets, address receiver) external;
-    function repay(uint256 assets, address receiver) external;
-    function pullDebt(address from, uint256 assets) external returns (bool);
-    function liquidate(address violator, address collateral) external;
+    function borrow(uint256 assets, address receiver) external {
+        require(totalAssets >= assets, "Not enough assets in the contract");
+        totalAssets -= assets;
+        debts[receiver] += assets;
+        lastBorrower = receiver;
+    }
 
-    // [ASSIGNMENT]: don't forget that the following functions must be overridden in order to support borrowing:
-    // [ASSIGNMENT]: - disableController()
-    // [ASSIGNMENT]: - checkAccountStatus()
-    // [ASSIGNMENT]: - maxWithdraw()
-    // [ASSIGNMENT]: - maxRedeem()
-    // [ASSIGNMENT]: - _convertToShares()
-    // [ASSIGNMENT]: - _convertToAssets()
+    function repay(uint256 assets, address receiver) external {
+        require(debts[receiver] >= assets, "Not enough debt to repay");
+        require(balances[receiver] >= assets, "Not enough balance to repay");
+        totalAssets += assets;
+        debts[receiver] -= assets;
+        balances[receiver] -= assets;
+    }
 
-    // [ASSIGNMENT]: don't forget about implementing and using modified version of the _msgSender() function for the
-    // borrowing purposes
+    function pullDebt(address from, uint256 assets) external returns (bool) {
+        require(debts[from] >= assets, "Not enough debt to pull");
+        debts[from] -= assets;
+        return true;
+    }
 
-    // [ASSIGNMENT] optional: add interest accrual
-    // [ASSIGNMENT] optional: integrate with an oracle of choice in checkAccountStatus() and liquidate()
-    // [ASSIGNMENT] optional: implement a circuit breaker in checkVaultStatus(), may be EIP-7265 inspired
-    // [ASSIGNMENT] optional: add EIP-7540 compatibility for RWAs
+    function liquidate(address violator) external {
+        require(debts[violator] > 0, "No debt to liquidate");
+        totalAssets += debts[violator];
+        debts[violator] = 0;
+    }
+
+    function disableController() external {
+        // Disable the controller
+        controllerEnabled = false;
+    }
+
+    function checkAccountStatus() external {
+        // Check the account status
+        if (balances[msg.sender] < debts[msg.sender]) {
+            // The account is underfunded
+        } else {
+            // The account is in good standing
+        }
+    }
+
+    function maxWithdraw() external {
+        // Determine the maximum withdrawal amount
+        uint256 maxWithdrawAmount = balances[msg.sender] - debts[msg.sender];
+    }
+
+    function maxRedeem() external {
+        // Determine the maximum redeem amount
+        uint256 maxRedeemAmount = balances[msg.sender] - debts[msg.sender];
+    }
+
+    function _convertToShares(uint256 assets) internal {
+        require(totalAssets > 0 && totalShares > 0, "Cannot convert to shares");
+        return (assets * totalShares) / totalAssets;
+    }
+
+    function _convertToAssets(uint256 shares) internal {
+        require(totalAssets > 0 && totalShares > 0, "Cannot convert to assets");
+        return (shares * totalAssets) / totalShares;
+    }
+
+    function _msgSender() internal view returns (address) {
+        // Return the last borrower for borrowing purposes
+        return lastBorrower;
+    }
 }
